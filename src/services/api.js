63bfdf7b0af3e606df56cf85b6eca13101022b76
Abhradip Saha <110524706+abhradip-saha@users.noreply.github.com>
2024-01-15 23:44:37 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL='https://abhradip-file.onrender.com/'
+const API_URL='https://abhradip-file.onrender.com'
 
 export const uploadFile = async (data)=>{
     try {
